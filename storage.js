c9afbcad001dfdb2316f3cee2d8cb714fa32298b
NSSMac2009EC <nssmac2009ec@NSSMac2009ECs-MacBook-Pro.local>
2018-07-16 15:59:02 -0500

    added comments to js and items to database

--- storage.js
+++ storage.js
@@ -1,8 +1,12 @@
+////////////////// Furniture
+
 const writingDesk = {
     name: "Shaker Writing Desk",
     location: "Bedroom",
     description: "This antique desk is special because I found and purchased it with my wife at an Ohio Amish auction."
 }
+
+////////////////// Crafts
 
 const vintageInkwell = {
     name: "Vintage Ink Well",
@@ -10,11 +14,21 @@ const vintageInkwell = {
     description: "I enjoy this inkwell because it belonged to my grandfather and holds enough ink to survive weeks of writing."
 }
 
+////////////////// Electronics
+
 const gamingDesktop = {
     name: "PC Master Race Mothership",
     location: "Computer Desk",
-    description: "Makes the supercomputers of the 1950s look like stone tablets"
+    description: "Makes the supercomputers of the 1950s look like stone tablets."
 }
+
+const recordingEquipment = {
+    name: "Scarlett Focusrite",
+    location: "Computer Desk",
+    description: "The premiere audio interface for musicians."
+}
+
+////////////////// Database
 
 const HIDatabase = {
     furniture : [],
@@ -25,5 +39,6 @@ const HIDatabase = {
 HIDatabase.crafts.push(vintageInkwell);
 HIDatabase.furniture.push(writingDesk);
 HIDatabase.electronics.push(gamingDesktop);
+HIDatabase.electronics.push(recordingEquipment);
 
 console.log(HIDatabase)
