104eb759a96bea842aca0fb150bf9851c2808ed2
NSSMac2009EC <nssmac2009ec@NSSMac2009ECs-MacBook-Pro.local>
2018-07-17 09:04:24 -0500

    Completed Assignment

--- storage.js
+++ storage.js
@@ -6,12 +6,36 @@ const writingDesk = {
     description: "This antique desk is special because I found and purchased it with my wife at an Ohio Amish auction."
 }
 
+const officeChair = {
+    name: "Office Chair",
+    location: "Living room",
+    description: "The most comfortable swively chair I defintely don't always fall asleep in."
+}
+
+const computerDesk = {
+    name: "Computer Desk",
+    location: "Living room",
+    description: "Something that definitely needs to be upgraded in the future."
+}
+
 ////////////////// Crafts
 
 const vintageInkwell = {
     name: "Vintage Ink Well",
     location: "Writing desk",
     description: "I enjoy this inkwell because it belonged to my grandfather and holds enough ink to survive weeks of writing."
+}
+
+const fountainPen = {
+    name: "Fountain Pen",
+    location: "Writing desk",
+    description: "Run of the mill fountain pen found at Walmart"
+}
+
+const beadingKit = {
+    name: "Jewelry Beading Kit",
+    location: "Bedroom",
+    description: "Failed dreams of being a handcrafting jewerly lie here."
 }
 
 ////////////////// Electronics
@@ -28,6 +52,18 @@ const recordingEquipment = {
     description: "The premiere audio interface for musicians."
 }
 
+const gamingKeyboard = {
+    name: "Ducky Limited Edition Mechanical Keyboard",
+    location: "Computer Desk",
+    description: "A quality mechanical keyboard, but not worth the price and definitely needs cleaning."
+}
+
+const gamingMonitor = {
+    name: "AVG 1440p 144hz Monitor",
+    location: "Computer Desk",
+    description: "A terrific monitor that consistently gets 60fps if not 100fps."
+}
+
 ////////////////// Database
 
 const HIDatabase = {
@@ -37,8 +73,14 @@ const HIDatabase = {
 }
 
 HIDatabase.crafts.push(vintageInkwell);
+HIDatabase.crafts.push(fountainPen);
+HIDatabase.crafts.push(beadingKit);
 HIDatabase.furniture.push(writingDesk);
+HIDatabase.furniture.push(officeChair);
+HIDatabase.furniture.push(computerDesk);
 HIDatabase.electronics.push(gamingDesktop);
 HIDatabase.electronics.push(recordingEquipment);
+HIDatabase.electronics.push(gamingKeyboard);
+HIDatabase.electronics.push(gamingMonitor);
 
 console.log(HIDatabase)
